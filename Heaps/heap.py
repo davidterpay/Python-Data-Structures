03b460d246c1d50cf7a4106473daeb449ecb59ae
davidterpay <david.terpay@gmail.com>
2018-08-09 12:59:53 -0500

    Done with heaps with exception of docs

--- Heaps/heap.py
+++ Heaps/heap.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('../')
 # importing BinarySearchTree class
-from Binary_Tree.binarytree import BinarySearchTree
+# from Binary_Tree.binarytree import BinarySearchTree
+import math
 '''
 Written by David Terpay
 This class will demonstrate some of the functionality of a (min)heap. 
@@ -22,19 +23,84 @@ is remove the minimum element) in amortized O(1) time.
 '''
 
 
-class Heap(BinarySearchTree):
+class Heap():
     def __init__(self):
-        self.root = None
-        self.array = [None, None]
+        self.array = [None]
         self.size = 0
-        self.capacity = 0
+        self.capacity = 1
     
     def insert(self, data):
         if self.size == self.capacity:
             self.growArray()
-        self.size += 1
-        self.array[self.size] = data
-        self.heapifyUp(self.size)
+        if self.size == 0:
+            self.array.append(data)
+            self.size += 1
+        else:
+            self.size += 1
+            self.array[self.size] = data
+            self.heapifyUp(self.size)
+
+    def heapifyUp(self, index):
+        '''
+        Heapify up rebalances or fixes the heap from the insertion 
+        point up to the root if necessary. It checks the parent and 
+        swaps if necessary.
+
+        Runtime -- O(lg(n)) -- Because we might have to go up to
+        the entire height of the tree.
+        '''
+        if index > 1:
+            if (self.array[index] < self.array[self.parent(index)]):
+                self.swap(index, self.parent(index))
+                self.heapifyUp(self.parent(index))
+
+    def heapifyDown(self, index=1):
+        '''
+        We start at the root and build downward.
+
+        Runtime -- O(lg(n)) -- We might have to go down to the very bottom of
+        our tree.
+        '''
+
+        if not self.__isLeaf(index):
+            minChildIndex = self.__minChild(index)
+            if (self.array[index] > self.array[minChildIndex]):
+                self.swap(index, minChildIndex)
+                self.heapifyDown(minChildIndex)
+
+    def __minChild(self, index):
+        '''
+        Used for heapifydown
+        '''
+        if self.rightChild(index) <= self.size:
+            if self.array[self.rightChild(index)] < self.array[self.leftChild(index)]:
+                return self.rightChild(index)
+        return self.leftChild(index)
+
+    def __isLeaf(self, index):
+        '''
+        checks if is a leaf
+        '''
+        return index * 2 > self.size
+
+    def swap(self, low, upper):
+        '''
+        Swaps the data of the nodes in low and upper.
+        '''
+        first = self.array[low]
+        self.array[low] = self.array[upper]
+        self.array[upper] = first
+
+    def growArray(self):
+        '''
+        Adds another level of our model tree copys the data.
+        This runs in O(1) amortized time.
+        '''
+        
+        self.capacity += int(pow(2, math.log2(self.capacity + 1)))
+        newArray = [None for i in range((self.capacity))]
+        newArray[0:self.size] = self.array
+        self.array = newArray
     
     def remove(self):
         '''
@@ -47,47 +113,25 @@ class Heap(BinarySearchTree):
         ''' 
         return self.__removeMin()
 
+    def __removeMin(self):
+        '''
+        This automatically removes the min or max depending
+        on whether it is a max or min prioirity heap.
+        '''
+        min = self.array[1]
+        self.array[1] = self.array[self.size]
+        self.array[self.size] = None
+        self.size -= 1
+        self.heapifyDown()
+        return min
+
     def isEmpty(self):
         '''
         checks if heap is empty
         '''
-        pass
-        
-    def heapifyUp(self, index):
-        '''
-        Heapify up rebalances or fixes the heap from the insertion 
-        point up to the root if necessary. It checks the parent and 
-        swaps if necessary.
+        return not self.array[1]
 
-        Runtime -- O(lg(n)) -- Because we might have to go up to
-        the entire height of the tree.
-        '''
-        if index > 1:
-            if (self.array[index] < self.array[self.parent(index)]):
-                self.swap(self.array[index], self.array[self.parent(index)])
-                self.heapifyUp(self.parent(index))
-
-    
-    def heapifyDown(self, index = 1):
-        '''
-        We start at the root and build downward.
-
-        Runtime -- O(lg(n)) -- We might have to go down to the very bottom of
-        our tree.
-        '''
-        if not self.__isLeaf(index):
-            minChildIndex = self.__minChild(index)
-            if (self.array[index] > self.array[minChildIndex]):
-                self.swap(index,minChildIndex)
-                self.heapifyDown(minChildIndex)
-
-    def swap(self, low, upper):
-        '''
-        Swaps the data of the nodes in low and upper.
-        '''
-        pass
-
-    def buildHeap(self):
+    def buildHeap(self, lst):
         '''
         Builds a heap from an existing list of data.
         Option 1: sort the array --> O(nlg(n)) if using mergesort, etc.
@@ -100,54 +144,63 @@ class Heap(BinarySearchTree):
             leaf nodes and work our way up. Half of our array is already a heap.
             We have to heapifydown on half of our array.
         '''
-        pass
+        self.array.extend(lst)
+        self.size = len(lst)
+        height = 0
+        while pow(2, height + 1) - 1 < self.size:
+            height += 1
+        self.capacity = pow(2, height + 1) - 1
+        self.array.extend([None] * (self.capacity - self.size))
+        for index in range(self.parent(self.size),0,-1):
+            self.heapifyDown(index)
 
-    def __minChild(self,index):
+    def heapSort(self, lst):
         '''
-        Used for heapifydown
+        first build the heap. second remove min n times. place min
+        at the end. last step is reversing the array. using index 0
+        as our start.
+        in memory, stable sort.
+        buildheap -- O(n) --
+        removeMin -- O(nlg(n)) --
+        reverse -- O(n) --
         '''
-        pass
+        self.buildHeap(lst)
+        prevSize = self.size
+        for __ in range(self.size):
+            min = self.array[1]
+            self.array[1] = self.array[self.size]
+            self.array[self.size] = min
+            self.size -= 1
+            self.heapifyDown()
+        for low in range(1,(prevSize + 1)//2):
+            self.swap(low, prevSize - low)
+        self.size = prevSize
+        return self.array[1:self.size]
 
-    def __isLeaf(self, index):
-        '''
-        checks if is a leaf
-        '''
-        pass
-
-    def growArray(self):
-        '''
-        Doubles the size of curr array and copys the data.
-        This runs in O(1) amortized time.
-        '''
-        pass
-
-    def __removeMin(self):
-        '''
-        This automatically removes the min or max depending
-        on whether it is a max or min prioirity heap.
-        '''
-        min = self.array[1]
-        self.array[1] = self.array[self.size]
-        self.size -= 1
-        self.heapifyDown()
-        return min
-    
-    def leftChild(self):
+    def leftChild(self, index):
         '''
         left child is 2 * index if rooted at 1 index.
         '''
-        pass
+        return index * 2
     
-    def rightChild(self):
+    def rightChild(self, index):
         '''
         right child is 2 index + 1 if rooted at 1 index.
         '''
-        pass
+        return index * 2 + 1
     
     def parent(self, index):
         '''
         parent is index // 2 if rooted at 1 index.
         '''
-        pass
+        return index // 2
+    
+    def __str__(self):
+        string = 'Size is : ' + str(self.size) + '\nCapacity is : ' + str(self.capacity)
+        string += '\nArray : ' + str(self.array)
+        return string
+
     
 
+heap = Heap()
+print(heap.heapSort([30, 20, 0, 8, 9, 7,23,4,19,1,-234,-1,-9043]))
