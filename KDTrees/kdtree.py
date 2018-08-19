352c3c351533f972de6f03956bdd9afcfc3d7db6
davidterpay <david.terpay@gmail.com>
2018-08-19 14:31:05 -0500

    I think i am done with remove

--- KDTrees/kdtree.py
+++ KDTrees/kdtree.py
@@ -42,12 +42,38 @@ class KDTree(BinarySearchTree):
 
     def remove(self, data):
         if self.root:
-            self.__remove(data)
+            self.__remove(data, self.root, 0)
         
-    def __remove(self, data):
-        pass
+    def __remove(self, data, node, dim):
+        if not node:
+            return None
+        if node.getData() == data:
+            parent = node.getParent()
+            if node.getRight(): # right child exists
+                findMin = self.findMin(node.getRight(), (node.getDimDis() + 1) % self.dimensions, node.getDimDis())
+                node.setData(findMin.getData())
+                right = self.__remove(node.getData(), node.getRight(), (node.getDimDis() + 1) % self.dimensions)
+                if right:
+                    right.setParent(node)
+                node.setRight(right)
+            elif node.getLeft(): # left child exists
+                findMin = self.findMin(node.getleft(), (node.getDimDis() + 1) % self.dimensions, node.getDimDis())
+                node.setData(findMin.getData())
+                right = self.__remove(node.getData(), node.getLeft(), (node.getDimDis() + 1) % self.dimensions)
+                if right:
+                    right.setParent(node)
+                node.setRight(right)
+            else: # zero child
+                node = None
+            if not parent:
+                self.root = node
+        elif node.getData()[dim] < data[dim]:
+            node.setRight(self.__remove(data, node.getRight(), (dim + 1) % self.dimensions))
+        else:
+            node.setLeft(self.__remove(data, node.getLeft(), (dim + 1) % self.dimensions))   
+        return node
     
-    def findMin(self, node, cd, dim):
+    def findMin(self, node, cd = 0, dim = 0):
         if not node:
             return None
         elif cd == dim:
@@ -63,10 +89,17 @@ class KDTree(BinarySearchTree):
         return reduce(lambda x,y: x if x.getData()[dimension] <= y.getData()[dimension] else y, [node for node in args if node])
 
     def find(self, data):
-        return self.__find(data)
+        return self.__find(data, self.root, 0)
     
-    def __find(self, data):
-        pass
+    def __find(self, data, node, dim):
+        if not node:
+            return None
+        if node.getData() == data:
+            return node
+        elif node.getData()[dim] < data[dim]:
+            return self.__find(data, node.getRight(), (dim + 1) % self.dimensions)
+        else:
+            return self.__find(data, node.getLeft(), (dim + 1) % self.dimensions)
 
     def createBalancedTree(self):
         pass
@@ -87,10 +120,15 @@ class KDTree(BinarySearchTree):
         pass
 
 from random import randint
-kdtree = KDTree(2)
-lst = [(0,10),(10,0),(-10,0),(-30,14),(-15,-30),(60,200),(50,730),(-100,-100),(-300,50)]
+kdtree = KDTree(1)
+# lst = [(0,10),(10,0),(-10,0),(-30,14),(-15,-30),(60,200),(50,730),(-100,-100),(-300,50)]
+lst = [[10],[0],[20]]
 kdtree.insertList(lst)
 print(kdtree)
 print('\n\n\n')
-print(kdtree.findMin(kdtree.root, 0, 0))
+kdtree.remove([10])
+print('\n\n\n')
+print(kdtree)
+print('\n\n\n')
+print(kdtree.root)
 
