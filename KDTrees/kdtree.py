6c913b9aba6150126eec3ed5fa80347e9366f830
davidterpay <david.terpay@gmail.com>
2018-08-20 15:15:40 -0500

    Done with sorting portion; Onto NN and range based searches

--- KDTrees/kdtree.py
+++ KDTrees/kdtree.py
@@ -13,23 +13,24 @@ class KDTree(BinarySearchTree):
 
     def insert(self, data):
         if self.root:
-            self.__insert(data, self.root, 0)
+            node = KDTreeNode(data, len(data))
+            self.__insert(node, self.root, 0)
         else:
             self.root = KDTreeNode(data, 0)
         
-    def __insert(self, data, node, dim):
-        if self.smallerDimValue(data, node, dim):
+    def __insert(self, newNode, node, dim):
+        if self.smallerDimValue(newNode, node, dim):
             if node.getLeft():
-                self.__insert(data, node.getLeft(), (dim + 1) % self.dimensions)
+                self.__insert(newNode, node.getLeft(), (dim + 1) % self.dimensions)
             else:
-                newNode = KDTreeNode(data, (dim + 1) % self.dimensions)
+                newNode.setDimDis((dim + 1) % self.dimensions)
                 newNode.setParent(node)
                 node.setLeft(newNode)
         else:
             if node.getRight():
-                self.__insert(data, node.getRight(), (dim + 1) % self.dimensions)
+                self.__insert(newNode, node.getRight(), (dim + 1) % self.dimensions)
             else:
-                newNode = KDTreeNode(data, (dim + 1) % self.dimensions)
+                newNode.setDimDis((dim + 1) % self.dimensions)
                 node.setRight(newNode)
                 newNode.setParent(node)
 
@@ -37,8 +38,8 @@ class KDTree(BinarySearchTree):
         for data in lst:
             self.insert(data)
 
-    def smallerDimValue(self, data, node, dim):
-        return node.getData()[dim] >= data[dim]
+    def smallerDimValue(self, newNode, node, dim):
+        return node.getData()[dim] >= newNode.getData()[dim]
 
     def remove(self, data):
         if self.root:
@@ -102,33 +103,83 @@ class KDTree(BinarySearchTree):
         else:
             return self.__find(data, node.getLeft(), (dim + 1) % self.dimensions)
 
-    def createBalancedTree(self):
-        pass
+    def createBalancedTree(self, lst):
+        data = self.quickselect(lst)
+        self.root = self.__buildTree(0, len(data) - 1, data, 0)
+        return list(map(KDTreeNode.getData, data))
 
-    def sort(self):
-        pass
+    def __buildTree(self, left, right, lst, dim):
+        if left > right:
+            return
+        median = (left + right) // 2
+        root = lst[median]
+        root.setDimDis(dim)
+        right = self.__buildTree(median + 1, right, lst, (dim + 1) % self.dimensions)
+        if right:
+            right.setParent(root)
+        left = self.__buildTree(left, median - 1, lst, (dim + 1) % self.dimensions)
+        if left:
+            left.setParent(root)
+        root.setRight(right)
+        root.setLeft(left)
+        return root
 
-    def quicksort(self):
-        pass
+    def __sort(self, lst, left, right, dim):
+        if left < right:
+            median = (left + right) // 2
+            # We need pivot to correctly put median in the middle
+            # So that we can properly sort the other dimensions and halves
+            self.pivot(lst, left, right, median, dim)
+            # Recursively sorting the right and left sides on the array
+            self.__sort(lst, left, median - 1, (dim + 1) % self.dimensions)
+            self.__sort(lst, median + 1, right, (dim + 1) % self.dimensions)
+            return lst
 
-    def shouldReplace(self):
-        pass
+    def pivot(self, lst, left, right, median, dim):
+        # We check if the partition is greater than or equal to median. If
+        # so then we have to recur on the proper side.
+        pivot = self.partition(left, right, lst, dim)
+        if pivot != median:
+            if pivot < median:
+                self.pivot(lst, pivot + 1, right, median, dim)
+            elif pivot > median:
+                self.pivot(lst, left, pivot - 1, median,  dim)
+        
+    def partition(self, left, right, lst, dim):
+        low = left - 1
+        for x in range(left, right):
+            if self.smallerDimValue(lst[x], lst[right], dim):
+                low += 1
+                temp = lst[low]
+                lst[low] = lst[x]
+                lst[x] = temp
+        low += 1
+        temp = lst[right]
+        lst[right] = lst[low]
+        lst[low] = temp
+        return low
 
-    def euclideanDistance(self):
-        pass
+    def quickselect(self, lst):
+        data = [KDTreeNode(value, 0) for value in lst]
+        return self.__sort(data, 0, len(data) - 1, 0)
+
+    def shouldReplace(self, target, currBest, potential):
+        potentialDistance = self.euclideanDistance(target, potential)
+        currentDistance = self.euclideanDistance(target, currBest)
+        return True if potentialDistance <= currentDistance else False
+
+    def euclideanDistance(self, first, second):
+        distance = 0
+        for index, data in enumerate(first.getData()):
+            distance += pow(data - second.getData()[index], 2)
+        return pow(distance, 1/2)
     
     def findNearestNeighbor(self):
         pass
 
 from random import randint
 kdtree = KDTree(2)
-lst = [(0,10),(10,0),(-10,0),(-30,14),(-15,-30),(60,200),(50,730),(-100,-100),(-300,50)]
-kdtree.insertList(lst)
+lstData = [(3, 2), (4, 4), (5, 8), (6, 1), (9, 0), (1, 1), (2, 2), (8, 7)]
+print(kdtree.createBalancedTree(lstData))
 print(kdtree)
-print('\n\n\n')
-kdtree.remove((0, 10))
-print('\n\n\n')
-print(kdtree)
-print('\n\n\n')
-print(kdtree.root)
-
+print(kdtree.root.getRight().getRight().getRight())
