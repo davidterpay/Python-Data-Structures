dd20a6fe44ff27ebe4fa0631e50ae92aa7b6d1c2
davidterpay <david.terpay@gmail.com>
2018-08-29 19:36:56 -0500

    Done with helper classes

--- BTrees/btreenode.py
+++ BTrees/btreenode.py
@@ -38,6 +38,75 @@ class BTreeNode():
         else:
             self.__insertSorted(0, len(self.data) - 1, newDataPair)
     
+    def insertList(self, lst):
+        '''
+        This function will allow user to input a list of datapair objects and 
+        will sort them and initialize the data member of this node.
+        INPUT:
+            lst: List of DataPair objects
+        OUTPUT:
+            New node
+        
+        Runtime - O(nlg(n)) - Since mergesort takes O(nlg(n)) time our
+        insert list will also run in that time.
+        '''
+
+        self.mergesort(lst)
+
+    def mergesort(self, lst):
+        '''
+        Merge sort is a sorting algorithm that splits the list of numbers
+        if half and then rebuilds the array upward. Once the size of each list
+        is = 1, we know that the list is sorted. Next, we simply merge two sorted 
+        lists and return the list. This is premise behind merge sort.
+        INPUT:
+            lst: List of data
+        OUTPUT:
+            Sorted list of data
+        
+        Runtime - O(nlg(n)) - Since it takes O(lg(n)) to divide the list and O(n) to
+        rebuild our array, our algorithm takes a total of O(nlg(n)) time.
+        '''
+
+        self.data = self.__mergesort(lst)
+    
+    def __mergesort(self, lst):
+        '''
+        Merge sort is a sorting algorithm that splits the list of numbers
+        if half and then rebuilds the array upward. Once the size of each list
+        is = 1, we know that the list is sorted. Next, we simply merge two sorted
+        lists and return the list. This is premise behind merge sort.
+        '''
+
+        if len(lst) == 1:
+            return lst
+        else:
+            median = (len(lst)) // 2
+            left = lst[0: median]
+            right = lst[median: len(lst)]
+            newLeft = self.__mergesort(left)
+            newRight = self.__mergesort(right)
+            return self.__merge(newLeft, newRight)
+
+    def __merge(self, leftlist, rightlist):
+        '''
+        Helper function to merge leftlist and rightlist into
+        one single list. It is assumed that the two lists are sorted.
+        '''
+
+        ctr = 0
+        while len(rightlist) != 0:
+            data = rightlist[0]
+            if ctr > len(leftlist) - 1:
+                leftlist.append(data)
+                rightlist.pop(0)
+            elif data < leftlist[ctr] or data == leftlist[ctr]:
+                leftlist.insert(ctr, data)
+                rightlist.pop(0)
+            else:
+                ctr += 1
+        return leftlist       
+
     def __insertSorted(self, left, right, datapair):
         '''
         Helps binary search our treenode to find the correct index to 
@@ -87,3 +156,4 @@ class BTreeNode():
         '''
 
         return len(self.data)
+
